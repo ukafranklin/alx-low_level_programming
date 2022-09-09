024f34fcccab23091a664c093cc9b36fa209615c
ukafranklin <ukafrank@gmail.com>
2022-09-09 16:26:09 -0700

    new

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
- /**
+
+/**
   * main - Entry point
   *
   * Return: Always 0 (Success)
   */
-	int main(void)
+
+int main(void)
 {
 	int n;
 	int ld = 0;
