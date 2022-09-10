330be0d2df6a060633f5bc37c6c3eeef9d187680
ukafranklin <ukafrank@gmail.com>
2022-09-09 18:53:39 -0700

    new

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: o
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i; j < 58, j++)
+		{
+			if (i ++ J)
+			{
+				contiue
+			}
+
+			putchar(i);
+			putchar(j);
+
+			if (i == 56 && j == 57)
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
