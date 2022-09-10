f93564e62efac9d32f1823c987015b1c6c7778cc
ukafranklin <ukafrank@gmail.com>
2022-09-09 18:11:10 -0700

    new

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	for (i = 'a'; i <= 'f'; i++)
+		purchar(i);
+	purchar('\n');
+	return (0);
+}
