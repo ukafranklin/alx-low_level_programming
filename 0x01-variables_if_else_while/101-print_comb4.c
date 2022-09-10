a8face0851bb88b3faa9f080b09a7b06c61a8678
ukafranklin <ukafrank@gmail.com>
2022-09-09 19:17:09 -0700

    new

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - prints out all possible combos of 3 digits
+ * main - Entry point
+ *
  * Return: 0
  */
 int main(void)
 {
-
 	int i, j, k;
 
 	for (i = 48; i < 58; i++)
 	{
 		for (j = i; j < 58; j++)
 		{
-			for (k = j; k < 58; k++)
+			for (k = j, k < 58; k++)
 			{
 				if (i == j || j == k || i == k)
 				{
@@ -32,7 +32,6 @@ int main(void)
 					putchar(',');
 					putchar(' ');
 				}
-
 			}
 		}
 	}
