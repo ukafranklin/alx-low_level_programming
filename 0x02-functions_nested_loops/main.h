01297671222f749299376e67b71372bc06af271f
ukafranklin <ukafrank@gmail.com>
2022-09-14 08:54:11 -0700

    new

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,6 @@ int print_sign(int n);
 int _abs(int ab);
 int print_last_digit(int ld);
 void jack_bauer(void);
-void print_times_table(void);
+void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
